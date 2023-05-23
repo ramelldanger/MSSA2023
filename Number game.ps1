0fbb0351564c3f444ff2e509e89d265367af23f7
ramell dangerfield <ramell.dangerfield83@outlook.com>
2023-05-23 04:32:29 -0700

    number game

---
+++ Number game.ps1
@@ -0,0 +1,7 @@
+﻿$NumberToGuess = 1..100 | Get-Random
+do {
+  [int]$UserGuess = Read-Host -prompt "Please enter a number between 1 and 100" 
+  if ($UserGuess -lt $NumberToGuess) {Write-Host "Pick a higher number" }
+  elseif ($UserGuess -gt $NumberToGuess) {Write-Host "Pick a lower number" }
+  else {write-host "congrats! you picked the right number" } 
+} until ($NumberToGuess -eq $UserGuess)
